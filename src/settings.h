e3292cb0ca5466bcc99a8bedda42b9b48288a51e
merspieler <merspieler@users.noreply.github.com>
2017-07-14 00:29:33 +0200

    src/settings.h: changed type of most vars to int
    Signed-off-by: merspieler <merspieler@users.noreply.github.com>

--- src/settings.h
+++ src/settings.h
@@ -9,18 +9,18 @@ class Settings
 
 	char TGA_FILE[256];
 
-	char TGA_ID;
-	char TGA_PALETTE;
-	char TGA_IMG_TYPE;
-	char TGA_PALETTE_START[2];
-	char TGA_PALETTE_END[2];
-	char TGA_PALETTE_SIZE;
-	char TGA_ORIGIN_X[2];
-	char TGA_ORIGIN_Y[2];
-	char TGA_WIDTH[2];
-	char TGA_HEIGHT[2];
-	char TGA_BITS_PER_PXIXEL;
-	char TGA_ATTR;
+	int TGA_ID;
+	int TGA_PALETTE;
+	int TGA_IMG_TYPE;
+	int TGA_PALETTE_START;
+	int TGA_PALETTE_END;
+	int TGA_PALETTE_SIZE;
+	int TGA_ORIGIN_X;
+	int TGA_ORIGIN_Y;
+	int TGA_WIDTH;
+	int TGA_HEIGHT;
+	int TGA_BITS_PER_PIXEL;
+	int TGA_ATTR;
 
 public:
 	Settings();
@@ -36,18 +36,18 @@ public:
 	bool setTGA_FILE(char file[256]);
 	void getTGA_FILE(char &ret);
 
-	char getTGA_ID();
-	char getTGA_PALETTE();
-	char getTGA_IMG_TYPE();
-	void getTGA_PALETTE_START(char &ret);
-	void getTGA_PALETTE_END(char &ret);
-	char getTGA_PALETTE_SITE();
-	void getTGA_ORIGIN_X(char &ret);
-	void getTGA_ORIGIN_Y(char &ret);
-	void getTGA_WIDTH(char &ret);
-	void getTGA_HEIGHT(char &ret);
-	char getTGA_BIST_PER_PIXEL();
-	char getTGA_ATTR();
+	int getTGA_ID();
+	int getTGA_PALETTE();
+	int getTGA_IMG_TYPE();
+	int getTGA_PALETTE_START();
+	int getTGA_PALETTE_END();
+	int getTGA_PALETTE_SITE();
+	int getTGA_ORIGIN_X();
+	int getTGA_ORIGIN_Y();
+	int getTGA_WIDTH();
+	int getTGA_HEIGHT();
+	int getTGA_BIST_PER_PIXEL();
+	int getTGA_ATTR();
 };
 
 #endif
