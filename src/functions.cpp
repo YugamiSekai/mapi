a6d298f6bf397f5435dd7e26e3651c49743edb59
merspieler <merspieler@users.noreply.github.com>
2017-07-13 23:22:21 +0200

    renamed functions and main file form .c to .cpp
    Signed-off-by: merspieler <merspieler@users.noreply.github.com>

---
+++ src/functions.cpp
@@ -0,0 +1,10 @@
+#include	"allinc.h"
+
+void print_help()
+{
+	printf("Useage: mapi [options]\n");
+	printf("Options:\n");
+	printf("  --help		Display this information\n");
+	printf("  -c			Char mode\n");
+	printf("  -i			Image mode\n");
+}
