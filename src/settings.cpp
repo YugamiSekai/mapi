47abb689c74c0094a00392abb67929642993f80e
merspieler <merspieler@users.noreply.github.com>
2017-07-14 11:31:42 +0200

    src/settings.cpp: added setter and getter methods
    Signed-off-by: merspieler <merspieler@users.noreply.github.com>

--- src/settings.cpp
+++ src/settings.cpp
@@ -2,11 +2,11 @@
 
 Settings::Settings()
 {
+	// initialisize with defaults
+	strcpy(&MODE, "b");
 	MAX_CHARS = DEFAULT_MAX_CHARS;
 	CHARTIME = DEFAULT_CHARTIME;
 	strcpy(LOGFILE, DEFAULT_LOGFILE);
-
-	strcpy(TGA_FILE, DEFAULT_TGA_FILE);
 
 	TGA_ID = DEFAULT_TGA_ID;
 	TGA_PALETTE = DEFAULT_TGA_PALETTE;
@@ -20,4 +20,88 @@ Settings::Settings()
 	TGA_HEIGHT = DEFAULT_TGA_HEIGHT;
 	TGA_BITS_PER_PIXEL = DEFAULT_TGA_BITS_PER_PIXEL;
 	TGA_ATTR = DEFAULT_TGA_ATTR;
+}
+
+bool Settings::setMAX_CHARS(int char_count)
+{
+	if(char_count > 0)
+	{
+		MAX_CHARS = char_count;
+		return true;
+	}
+	MAX_CHARS = DEFAULT_MAX_CHARS;
+	return false;
+}
+
+char Settings::getMODE()
+{
+	return MODE;
+}
+
+int Settings::getMAX_CHARS()
+{
+	return MAX_CHARS;
+}
+
+bool Settings::setCHARTIME(float time)
+{
+	if(time > 0)
+	{
+		CHARTIME = time;
+		return true;
+	}
+	CHARTIME = DEFAULT_CHARTIME;
+	return false;
+}
+
+float Settings::getCHARTIME()
+{
+	return CHARTIME;
+}
+
+bool Settings::setLOGFILE(char file[])
+{
+	strcpy(LOGFILE, file);
+	return true;
+}
+
+void Settings::getLOGFILE(char* ret)
+{
+	strcpy(ret, LOGFILE);
+}
+
+void Settings::getTGA_HEADER(char* header)
+{
+}
+
+bool Settings::setTGA_WIDTH(int width)
+{
+	if(width > 0)
+	{
+		TGA_WIDTH = width;
+		return true;
+	}
+	TGA_WIDTH = DEFAULT_TGA_WIDTH;
+	return false;
+}
+
+int Settings::getTGA_WIDTH()
+{
+	return TGA_WIDTH;
+}
+
+bool Settings::setTGA_HEIGHT(int height)
+{
+	if(height > 0)
+	{
+		TGA_HEIGHT = height;
+		return true;
+	}
+	TGA_HEIGHT = DEFAULT_TGA_HEIGHT;
+	return false;
+}
+
+int Settings::getTGA_HEIGHT()
+{
+	return TGA_HEIGHT;
 }
